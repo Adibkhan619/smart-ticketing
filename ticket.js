b69baabf08a1892acbbfdddd12df691c4f34dcc3
Adibkhan619 <work.adibkhan@gmail.com>
2024-02-18 23:23:03 +0600

    adding js functionalities

---
+++ ticket.js
@@ -0,0 +1,22 @@
+// get started button--------->
+// function getElementByClick(){
+//     const show =  document.getElementById('seat-selection');
+//     console.log(show);
+// }
+
+// function getElementByClick(){
+//     const element = document.getElementById(get-started);
+//     console.log(element);
+// }
+
+// function jumpToSection(sectionId){
+//     const targetSection = document.getElementById(sectionId);
+//     if(targetSection){
+//         targetSection.scrollIntoView;
+//     }
+// }
+
+function getSeatOnClick(elementId){
+    const selectedSeat = document.getElementById(elementId);
+    console.log('selected', elementId)
+}
